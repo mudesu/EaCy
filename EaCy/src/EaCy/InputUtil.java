877be814bf1a1a52b78bfb2f492d92110cbc0ba6
mudesu <harukana11060126@gmail.com>
2026-08-26 02:45:32 +0900

    EaCy

---
+++ EaCy/src/EaCy/InputUtil.java
@@ -0,0 +1,5 @@
+package EaCy;
+
+public class InputUtil {
+
+}
---
+++ EaCy/src/EaCy/Main.java
@@ -0,0 +1,14 @@
+package EaCy;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("簡易EC注文管理アプリケーション　EaCy");
+
+		System.out.println("メニュー一覧");
+		System.out.println("番号を入力");
+		int number = scanner.nextInt();
+	}
+}
