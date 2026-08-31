95e08ab09563690d6313626a66428b659dc36e2a
mudesu <harukana11060126@gmail.com>
2026-08-31 22:34:37 +0900

    Mainの改良2

--- EaCy/src/EaCy/Main.java
+++ EaCy/src/EaCy/Main.java
@@ -1,23 +1,55 @@
 package EaCy;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("簡易EC注文管理アプリケーション　EaCy");
 
-		while (true) {
-			System.out.println("メニュー一覧");
-			System.out.println("1：商品一覧");
-			System.out.println("2：カート内確認");
-			System.out.println("3：管理画面");
-			System.out.println("4：終了");
-			int number = InputUtil.inputInt("数字を入力してください");
+		ArrayList<Goods> goods = new ArrayList<>();
+		ArrayList<Cart> cart = new ArrayList<>();
 
+		while (true) {
+			int number = Menu.menu();
 			if (number == 1) {
+				System.out.println("商品一覧");
+				for (Goods no : goods) {
+					System.out.println("商品名:" + no.getTitle());
+					System.out.println("値段:" + no.getPrice());
+					System.out.println("在庫数:" + no.getStock());
+					System.out.println("カテゴリー:" + no.getCategory());
+				}
 				//一覧表示
 			} else if (number == 2) {
+				for (Cart no : cart) {
+					System.out.println("商品名:" + no.getTitle());
+					System.out.println("購入数:" + no.getQuantity());
+				}
 				//カート内表示
 			} else if (number == 3) {
+				while (true) {
+					System.out.println("管理画面");
+					System.out.println("1：商品追加");
+					System.out.println("2：商品情報更新");
+					System.out.println("3：商品削除");
+					System.out.println("4：終了");
+					int admin = InputUtil.inputInt("番号を入力");
+
+					if (admin == 1) {
+						//商品追加
+					} else if (admin == 2) {
+						//商品情報更新
+					} else if (admin == 3) {
+						//商品削除
+					} else if (admin == 4) {
+						System.out.println("メニューに戻ります。");
+						break;
+						//終了
+					} else {
+						System.out.println("1~4の番号を入力してください。");
+					}
+				}
 				//管理
 			} else if (number == 4) {
 				//終了
